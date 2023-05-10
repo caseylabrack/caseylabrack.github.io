8c891852097b04fe836becddcf537a6076850f74
Casey Labrack <caseyl@m-73270.myfiosgateway.com>
2023-05-10 10:40:17 -0400

    posts format, posts, and build script

--- build.lua
+++ build.lua
@@ -1,14 +1,16 @@
 siteF = io.output("index2.html")
 
 function post (p)
-	siteF:write("<a href='"..p.url.."'>")
 	siteF:write("<div class='post'>")
+	siteF:write("<a href='"..p.url.."'>")
+	siteF:write("<img src='./thumbs/"..p.thumbs.."' style='max-width:320px;' loading='lazy'/>")
 	siteF:write("<h2>"..p.name.."</h2>")
 	siteF:write("</a>")
 	siteF:write("<p class='tag'>")
 	for _,t in pairs(p.tags) do
 		siteF:write("#"..t)
 	end
+	siteF:write("<p class='description'>"..p.info.."</p>")
 	siteF:write("</div>")
 end
 
--- posts.lua
+++ posts.lua
@@ -1,27 +1,64 @@
 post{
+	thumbs="zaag.png",
 	name="Zaag",
 	url="",
 	tags={"toys"},
-	info="Zaag is pretending to be a lost 1980s vector arcade game. Blast the zoids, cleanse the tau.",
+	info="Videogame in the style of classic 1980s vector arcade shoot-'em-ups.",
 }
-
 post{
+	thumbs="maia-sleep.png",
+	name="Maia Learns to Sleep",
+	url="https://www.caseylabrack.com/sketches/maia-sleep",
+	tags={"dataviz"},
+	info="The full record of changings and feedings reveals how long it took baby Maia to start sleeping through the night.",
+}
+post{
+	thumbs="inequality-waterfall.png",	
 	name="Inequality Explorer",
-	url="",
+	url="https://caseylabrack.com/projects/inequality-explorer/",
 	tags={"dataviz"},
-	info="check it out",
+	info="What people earn, pay in taxes, get in benefits, and what is left over, by income group and year. From the supplemental data files of the Congressional Budget Office.",
+}
+post{
+	thumbs="perlin2.png",
+	name="Perlin noise portraits",
+	url="https://shastabolicious.itch.io/perlin-noise-portraits",
+	tags={"art"},
+	info="A fun perlin noise field visualizer.",
+}
+post{
+	thumbs="grocery-stores.png",
+	name="Availability of Real Grocery Stores in D.C.",
+	url="https://observablehq.com/@shastabolicious/availability-of-real-grocery-stores-in-d-c",
+	tags={"dataviz"},
+	info="Distance to the nearest grocery store for each neighborhood in D.C., an interesting social indicator.",
 }
 
 post{
+	thumbs="taxes@3x.png",
 	name="Tax Rates Pre- and Post-Reagan",
-	url="",
+	url="https://observablehq.com/@shastabolicious/the-evolution-of-tax-rates-in-america",
 	tags={"dataviz"},
-	info="Tax rates going back to 1962",
+	info="A visual history tax rates in America, from post-New Deal to Reagan, Bush, and today.",
 }
-
 post{
+	thumbs="gunshots@2x.png",
 	name="A Map of Gunshots in D.C.",
 	url="https://observablehq.com/@shastabolicious/gunshots-in-d-c-2018-early-2019/2",
 	tags={"dataviz"},
-	info="Map of all the gunshots D.C. police heard",
+	info="D.C. police have microphones throughout the city listening for gunshots. This map shows the density of gunshots by neighborhood.",
+}
+post{
+	thumbs="with-obama@3x.png",
+	name="With Obama",
+	url="",
+	tags={"art"},
+	info="A project to insert myself in the official White House photographs of the Obama administration, for funsies."
+}
+post{
+	thumbs="dinoblaster2.gif",
+	name="DinoBlaster",
+	url="http://store.steampowered.com/app/653960/DinoBlaster/",
+	tags={"toys"},
+	info="Videogame about sidestepping extinction as a brontosaurus, in the style of classic vector arcade games.",
 }
