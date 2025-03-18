c30c59318eadbfa368015471fb945f1c0efdb919
caseylabrack-cbo <casey.labrack@cbo.gov>
2025-03-18 18:47:55 -0400

    government revenues chart tweaks

--- projects/government-revenues/main.js
+++ projects/government-revenues/main.js
@@ -1,12 +1,12 @@
-const svg = d3.select("figure").append("svg").attr("viewBox", "0 0 1200 1500").attr("xmlns", "http://www.w3.org/2000/svg")
-    .attr("width", 1200).attr("height", 1500).lower(),
-    margin = {top: 0, bottom: 120, left: 25, right: 20},
+const svg = d3.select("figure").append("svg").attr("viewBox", "0 0 900 1500").attr("xmlns", "http://www.w3.org/2000/svg")
+    .attr("width", 900).attr("height", 1500).lower(),
+    margin = {top: 0, bottom: 120, left: 75, right: 20},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom,
     chart = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`),
     x = d3.scaleBand().domain(d3.range(2001,2032+1,1)).range([0,width]).padding(0.1),
     y = d3.scaleBand().range([0, height]).padding(0.5).align(.5),
-    color = d3.scaleDiverging().domain([-2.5,0,2.5]).interpolator(function(x){return x==.5 ? "white" : d3.interpolatePiYG(x)})
+    color = d3.scaleDiverging().domain([-2.5,0,2.5]).interpolator(x => x==.5 ? "white" : d3.interpolatePiYG(x)) // force white on exact middle
 
 d3.csv("data.csv").then(csv => {
     const data = csv.map(row => ({
@@ -15,7 +15,7 @@ d3.csv("data.csv").then(csv => {
         value: +row["value"]
     }))
 
-    // get uniques of titles, set as y domain
+    // get list of unique law titles, set as y domain
     {
         let reverseTable = {}
         for (const row of data) {
@@ -37,10 +37,10 @@ d3.csv("data.csv").then(csv => {
             .on("mouseenter", function(_, d) {
                 tooltip.text(d.value == 0 ? "0" : d.value.toFixed(1))
                     .attr("x", x(d.year) + x.bandwidth()/2)
-                    .attr("y", y(d.title) + y.bandwidth()/2)
+                    .attr("y", y(d.title) + y.bandwidth()/2 + 1)
                     .attr("fill", Math.abs(d.value) > 1.25 ? "white" : "black")
             })
-            .on("mouseleave", function(event, d) {
+            .on("mouseleave", () => {
                 tooltip.text("")
             })
 
@@ -60,28 +60,41 @@ d3.csv("data.csv").then(csv => {
         }
     })
 
-    chart.selectAll("text.labels")
+    const lawLabels = chart.selectAll("text.labels")
         .data(labelsData)
         .enter()
         .append("text").classed("labels", true)
             .text(d => d.title)
-            .attr("x", d => x(Math.round((d.minYear + d.maxYear)/2)))
+            .attr("x", d => { 
+                const center = (d.minYear + d.maxYear)/2
+                return x(Math.floor(center)) + x.bandwidth()/2 + (center % 1) * x.bandwidth()
+            })
             .attr("y", d => y(d.title) - 5)
+
+    // use tspans to add footnote flags on three laws with technical names
+    lawLabels
+        .filter(d => d.title == "Patient Protection and Affordable Health Care Act" 
+            || d.title == "Tax Cuts and Jobs Act, 2017" 
+            || d.title == "Inflation Reduction Act, 2022")
+        .append("tspan")
+            .text((_,i) => " " + (i + 1))
+            .attr("baseline-shift", "super")
+            .attr("font-size", ".7em")
 
     chart.append("g").call(d3.axisBottom(x).tickValues(d3.range(2000,2030+1,5))).attr("transform", `translate(0, ${height})`)
     
     svg.append("text")
         .text("Fiscal year")
-        .attr("x", width/2)
+        .attr("x", (x.range()[0] + x.range()[1])/2)
         .attr("y", height+margin.top + 30)
         .style("text-anchor", "middle")
         .style("dominant-baseline", "hanging")
         .style("font-size", ".8em")
 
     // legend
-    const legend = svg.append("g").attr("transform", `translate(${margin.left},${margin.top+height+60})`)
-    const legendWidth = 200 // width of the legend
-    const legendHeight = 20 // height of the legend box
+    const legend = svg.append("g").attr("transform", `translate(5,${margin.top+height+60})`)
+    const legendWidth = 200 
+    const legendHeight = 20 
     const legendData = d3.range(-3,3+.01,1)
     const legendX = d3.scaleBand().domain(legendData).range([0,legendWidth])
     legend.selectAll("rect")
@@ -90,7 +103,7 @@ d3.csv("data.csv").then(csv => {
         .append("rect").classed("chartcell", true)
             .attr("x", d => legendX(d))
             .attr("y", 10)
-            .attr("width", legendWidth/legendData.length)
+            .attr("width", legendX.bandwidth())
             .attr("height", legendHeight)
             .attr("fill", d => color(d))
     legend.append("g").call(d3.axisBottom(legendX)).attr("transform", `translate(0,${legendHeight+10})`)
